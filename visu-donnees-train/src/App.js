38045cb420723ee99802aa2e3977e162e4bdb88d
rouxyan <21461-rouxyan@users.noreply.gricad-gitlab.univ-grenoble-alpes.fr>
2024-12-04 15:00:07 +0100

    initialized Pie Chart

--- visu-donnees-train/src/App.js
+++ visu-donnees-train/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import TrainDataVisualization from "./components/TrainDataVisualization";
+import PieChart from "./components/PieChart";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <TrainDataVisualization />
+      {/* <PieChart /> */}
       </div>
   );
 }
